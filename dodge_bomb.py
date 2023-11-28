17162ba302971a195cf65d2769357e8c4d69ee9a
c0b22171 <c0b2217150@edu.teu.ac.jp>
2023-11-28 14:22:41 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,6 @@
-import sys
 import random
+import sys
 import pygame as pg
-
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -13,11 +12,14 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20,20))  # 爆弾 透明のサーフェイスを作る
-    bb_img.set_colorkey
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_rct = bb_img.get_rect()  # 練習2
-    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bb_img = pg.Surface((20, 20))   # 練習１：透明のSurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習１：赤い半径10の円を描く
+    bb_rct = bb_img.get_rect()  # 練習１：爆弾SurfaceのRectを抽出する
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 練習２：爆弾の速度
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -27,10 +29,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
