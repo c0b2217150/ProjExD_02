9a9c1ddb078b6f4be5ebd968bd65fa8e36eb17b6
c0b22171 <c0b2217150@edu.teu.ac.jp>
2023-11-28 14:51:34 +0900

    練習4 : 画面はみだし判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,21 @@ delta = {  # 練習３：押下キーと移動量の辞書
 }
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数 rct：こうかとんor爆弾SurfaceのRect
+    戻り値：横方向，縦方向はみ出し判定結果（画面内：True／画面外：False）
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向はみ出し判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向はみ出し判定
+        tate = False
+    return yoko, tate
+
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -46,8 +61,16 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)  # 練習３：こうかとんを移動させる
         bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出たら
+            vy *= -1
+        bb_rct.move_ip(vx, vy) 
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
